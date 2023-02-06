f412d5de125f09c1db2dfdfdd424bc0d7f01c7a5
Brayden Kim <dudgjs1318@gmail.com>
2023-02-05 19:18:50 -0800

    cors

--- server/server.js
+++ server/server.js
@@ -6,9 +6,16 @@ const mongoose = require('mongoose');
 const postRoutes = require('./routes/post')
 const userRoutes = require('./routes/user');
 const commentRoutes = require('./routes/comment');
+const cors = require('cors')
+
 const app = express();
 const PORT = process.env.PORT
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors({
+    origin: ['http://localhost:3000',
+        "https://apple-market.onrender.com"]
+}))
 
 app.get('/', (req, res) => {
     res.send('hello world')
