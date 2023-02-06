2fa1cab96e3476ebd6dc78c1b414d4d3f0bb79cd
Brayden Kim <dudgjs1318@gmail.com>
2023-02-05 21:38:00 -0800

    get back file structure

--- server/server.js
+++ server/server.js
@@ -26,11 +26,12 @@ app.use('/api/user', userRoutes);
 app.use('/api/comment', commentRoutes);
 
 
-// const __dirname = path.resolve();
+const _dirname = path.resolve()
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.use(express.static(path.join(_dirname, '/client/dist')));
 app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/client/dist/index.html'))
+    res.sendFile(path.join(_dirname, '/client/dist/index.html'))
 );
 
 mongoose.set("strictQuery", false);
